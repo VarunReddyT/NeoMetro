a1334d8de311c6ec4a3c715d9309100b01380bee
VarunReddyT <tvarun2014@gmail.com>
2025-06-15 11:42:06 +0530

    DockerDone

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoutes = require('./Routes/User.js'); 
 const ticketRoutes = require('./Routes/Ticket.js');
-// const chatRoutes = require('./Routes/Chat.js');
-// const stripeRoutes = require('./Routes/Stripe.js');
-// const subsIdRoutes = require('./Routes/SubsId.js');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -30,9 +27,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', userRoutes); 
 app.use('/api/tickets', ticketRoutes);
-// app.use('/api/chat',chatRoutes);
-// app.use('/api/stripe', stripeRoutes);
-// app.use('/api/subsid', subsIdRoutes);
 
 
 app.listen(port, () => {
